82839aee65622518090b7fd03c60359a801bb4ad
Abubakar506 <91673839+Abubakar506@users.noreply.github.com>
2024-11-06 00:42:55 +0500

    Update app.py

--- app.py
+++ app.py
@@ -17,16 +17,6 @@ from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-import nltk
-# Download required nltk data if not already available
-@st.cache_resource
-def ensure_nltk_data():
-    nltk.download('punkt')
-    nltk.download('popular', quiet=True)
-    nltk.download('nps_chat',quiet=True)
-    nltk.download('punkt') 
-    nltk.download('wordnet')
-ensure_nltk_data()
 # Google Calendar API scopes
 SCOPES = [
     'https://www.googleapis.com/auth/calendar',
